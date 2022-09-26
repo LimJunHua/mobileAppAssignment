4ec0f8b8c408b2d86180444f78e13e6f00ff8c4a
Adminstrator <limjh-wm18@student.tarc.edu.my>
2022-09-26 21:09:40 +0800

    Initial Commit

--- app/src/main/java/com/example/mobileassignment/signUp.kt
+++ app/src/main/java/com/example/mobileassignment/signUp.kt
@@ -16,14 +16,15 @@ class signUp : AppCompatActivity() {
             this, R.layout.activity_sign_up)
         //receive data
         val bundle = intent.extras
-        val tv1 = binding.tv1
-        val tv2 = binding.tv2
 
-        if (bundle != null){
-            tv1.text = " ${bundle.getString("username")}"
-            tv2.text = "${bundle.getString("password")}"
 
-        }
+
+
+        //if (bundle != null){
+          //  tv1.text = " ${bundle.getString("username")}"
+           // tv2.text = "${bundle.getString("password")}"
+
+        //}
 
     }
 }
