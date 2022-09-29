7b4080725261a3f7a98f11a41f6cfd746f37016b
cj choo <86222906+cjchootroopa@users.noreply.github.com>
2022-09-29 19:59:55 +0800

    small changes on UserHome

--- app/src/main/java/com/example/mobileassignment/UserHome.kt
+++ app/src/main/java/com/example/mobileassignment/UserHome.kt
@@ -7,14 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.mobileassignment.databinding.ActivityUserMainBinding
+import com.google.firebase.auth.FirebaseAuthException
 
 class UserHome : Fragment() {
     private lateinit var binding: ActivityUserMainBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = ActivityUserMainBinding.inflate(layoutInflater)
 
         return inflater.inflate(R.layout.fragment_user_home, container, false)
     }
+
+
 }
