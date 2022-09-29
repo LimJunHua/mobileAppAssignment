bb296f80f317727a9b8ee3cea299b3b1971d6e26
Adminstrator <limjh-wm18@student.tarc.edu.my>
2022-09-29 19:59:27 +0800

    Initial Commit

--- app/src/main/java/com/example/mobileassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobileassignment/MainActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.mobileassignment.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var databases : DatabaseReference
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,28 +20,44 @@ class MainActivity : AppCompatActivity() {
             this, R.layout.activity_main)
 
         val signup = binding.tvRegister
-        val username = binding.dataUsername
-        val password = binding.dataPassword
+        val username = binding.dataUsername.text.toString()
+        val password = binding.dataPassword.text.toString()
         val forgetPasswords = binding.tvForgetPassword
 
         signup.setOnClickListener {
             val myIntent = Intent(this,signUp::class.java )
-            //passdata
-            val bundle = Bundle()
-
-            bundle.putString("username", username.text.toString())
-            bundle.putString("password", password.text.toString())
-            myIntent.putExtras(bundle)
-
             startActivity(myIntent)
         }
         forgetPasswords.setOnClickListener {
             val myIntent = Intent(this,forgetPassword::class.java )
             startActivity(myIntent)
         }
-        val btnTest = binding.btnLogin
+        val btnLogin = binding.btnLogin
+
+        btnLogin.setOnClickListener(){
+            databases = FirebaseDatabase.getInstance().getReference("users")
+            val username = binding.dataUsername.text.toString()
+            databases = FirebaseDatabase.getInstance().getReference("users")
+            databases.child(username).get().addOnSuccessListener {
+                if (it.exists()) {
+                    var passwords = it.child("password").value.toString()
+                    if(password.equals(passwords)){
+                        val myIntent = Intent(this,forgetPassword::class.java )
+                        startActivity(myIntent)
+                    }
 
 
+                }
+            }
+
+        }
+
+        //passdata
+        // val bundle = Bundle()
+
+        //bundle.putString("username", username.text.toString())
+        // bundle.putString("password", password.text.toString())
+        //myIntent.putExtras(bundle)
 
     }
 
