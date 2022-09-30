c768fb8a71fbbf7885746b5879476239b6c14db9
cj choo <86222906+cjchootroopa@users.noreply.github.com>
2022-10-01 02:48:58 +0800

    changes on UserBook.ktand activity_user_book.xml

--- app/src/main/java/com/example/mobileassignment/UserBook.kt
+++ app/src/main/java/com/example/mobileassignment/UserBook.kt
@@ -1,67 +1,58 @@
 package com.example.mobileassignment
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import android.widget.DatePicker
 import android.app.AlertDialog
+import android.app.TimePickerDialog
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
 import com.example.mobileassignment.databinding.ActivityUserBookBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
 
-class UserBook: AppCompatActivity() {
+class UserBook: AppCompatActivity(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
     private lateinit var binding: ActivityUserBookBinding
-    private lateinit var database: DatabaseReference
+    private lateinit var databases: DatabaseReference
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var datePicker: DatePicker
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_user_book)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val database = FirebaseDatabase.getInstance()
 
-        var cal = Calendar.getInstance()
-        //DatePicker
-        // get the references from layout file
-
-        this.binding.tvDate
-        this.binding.btnDate
-        binding.tvDate.text = "--/--/----"
-
-        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
-                                   dayOfMonth: Int) {
-                cal.set(Calendar.YEAR, year)
-                cal.set(Calendar.MONTH, monthOfYear)
-                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-                updateDateInView()
-            }
+        //navigation
+        binding.btnHome.setOnClickListener(){
+            val intent = Intent(this, UserMainActivity::class.java)
+            startActivity(intent)
         }
 
-        binding.btnDate.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(view: View) {
-                DatePickerDialog(this@UserBook,
-                    dateSetListener,
-                    // set DatePickerDialog to point to today's date when it loads up
-                    cal.get(Calendar.YEAR),
-                    cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)).show()
-            }
+        //get date and time
+        pickDate()
 
-        })
+        var cal = Calendar.getInstance()
 
+
+        //get reason and venue
         val reason = arrayOf("Medical Checkup", "Feeling Unwell")
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, reason)
         binding.spinReason.adapter = adapter
+        val spinReason = binding.spinReason
 
         val venue = arrayOf("Klinik We-Care", "Klinik Vitality", "Klinik Utama")
         val adapt = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, venue)
         binding.spinVenue.adapter = adapt
+        val spinVenue = binding.spinVenue
 
         when (venue){
             arrayOf("Klinik We-Care") -> binding.tvClinicAddress.text = String.format("4, Jalan 5/4c, Taman Desa Melawati, 53100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur")
@@ -69,33 +60,110 @@ class UserBook: AppCompatActivity() {
             arrayOf("Klinik Utama") -> binding.tvClinicAddress.text = String.format("144, Jln Kepong, Pekan Kepong, 52100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur")
         }
 
-        binding.btnHome.setOnClickListener(){
-            val intent = Intent(this, UserMainActivity::class.java)
-            startActivity(intent)
-        }
 
 
 
-        binding.btnProfile.setOnClickListener(){
-            val intent = Intent(this, UserProfile::class.java)
-            startActivity(intent)
-        }
 
+        val myRef = database.getReference("bookings")
+
+        //book the appointment
         binding.btnBook2.setOnClickListener(){
+            val bundle = intent.extras
+            val spinReason = binding.spinReason.selectedItemPosition.toString()
+            val spinVenue = binding.spinVenue.selectedItemPosition.toString()
+            val tvTimePicker = binding.tvTimePicker.text.toString()
+
+
+
+            databases = FirebaseDatabase.getInstance().getReference("bookings")
+            databases.child(spinReason).get().addOnSuccessListener {
+
+            }
+
+
+
+            if (spinReason.isNotEmpty() || spinVenue.isNotEmpty() || tvTimePicker.isNotEmpty()) {
+
+            } else {
+                Toast.makeText(applicationContext,"Please fill up the details.", Toast.LENGTH_SHORT).show()
+            }
+
+
 
         }
-
-
-
-
 
     }
 
-    private fun updateDateInView() {
-        val myFormat = "MM/dd/yyyy" // mention the format you need
-        var cal = Calendar.getInstance()
-        val sdf = SimpleDateFormat(myFormat, Locale.US)
-        binding.tvDate.text = sdf.format(cal.getTime())
+
+
+//    private fun save(){
+//        val spinReason = binding.spinReason.selectedItemPosition.toString()
+//        val spinVenue = binding.spinVenue.selectedItemPosition.toString()
+//        val tvDate = binding.tvDate.text.toString()
+//
+//
+//        val database = FirebaseDatabase.getInstance()
+//        val myRef = database.getReference("bookingAppointment")
+//        myRef.child("booking").get().addOnSuccessListener {
+//            Toast.makeText(applicationContext, "Booking is successful!", Toast.LENGTH_LONG).show()
+//        }
+//            .addOnFailureListener {
+//                Toast.makeText(applicationContext, "Booking is failed", Toast.LENGTH_LONG).show()
+//            }
+//
+//    }
+
+
+
+
+    //TimePicker
+    var day = 0
+    var month = 0
+    var year = 0
+    var hour = 0
+    var minute = 0
+
+    var savedday = 0
+    var savedmonth = 0
+    var savedyear = 0
+    var savedHour = 0
+    var savedMinute = 0
+
+    private fun pickDate(){
+        binding.btnTimePicker.setOnClickListener(){
+            getDateTimeCalendar()
+
+            DatePickerDialog(this,this,year,month,day).show()
+        }
+
+    }
+
+    private fun getDateTimeCalendar(){
+        val cal :Calendar = Calendar.getInstance()
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        month = cal.get(Calendar.MONTH)
+        year = cal.get(Calendar.YEAR)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+
+        binding.tvTimePicker.text = "$savedday-$savedmonth-$savedyear $savedHour : $savedMinute"
+
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        savedday = dayOfMonth
+        savedmonth = month
+        savedyear = year
+
+        getDateTimeCalendar()
+
+        TimePickerDialog(this,this,hour,minute,true).show()
     }
 
 }
